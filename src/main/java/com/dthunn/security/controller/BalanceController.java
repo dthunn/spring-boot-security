65bf274f1835aa9def35d5008642071ff8f092e5
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-03-29 14:26:36 -0600

    sec 4 start

--- src/main/java/com/dthunn/security/controller/BalanceController.java
+++ src/main/java/com/dthunn/security/controller/BalanceController.java
@@ -4,11 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class WelcomeController {
+public class BalanceController {
 
-    @GetMapping("/welcome")
-    public  String sayWelcome () {
-        return "Welcome to Spring Application with security";
+    @GetMapping("/myBalance")
+    public  String getBalanceDetails () {
+        return "Here are the balance details from the DB";
     }
 
 }
